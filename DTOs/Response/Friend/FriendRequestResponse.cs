141d1962a993a93fdd2bc2b6b30677f41acca16b
Trong Tri <maitrongtri0101@gmail.com>
2026-04-03 13:02:13 +0700

    feat(relationship): API relationship

---
+++ DTOs/Response/Friend/FriendRequestResponse.cs
@@ -0,0 +1,10 @@
+namespace Kpett.ChatApp.DTOs.Response.Friend
+{
+    public class FriendRequestResponse
+    {
+        public string SenderId { get; set; } = null!;
+        public string ReceiverId { get; set; } = null!;
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
